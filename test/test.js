ad1941a2d3352026d52c095c78895a23b5bab095
ben91lin <ben91lin@gmail.com>
2021-08-07 17:26:47 +0800

    del temp test

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const Cwb = require('..').Cwb;
-
-(
-    async function() {
-        const cwb = new Cwb()
-        await cwb.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/C-B0025-001?Authorization=CWB-C6E1078B-4311-4044-BDC8-EBE6EC553F7B')
-        console.log(cwb)
-    }
-)()
