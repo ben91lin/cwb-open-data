bb131a5c1266f3cd8860d07c648a2b89c49a2ba7
ben91lin <ben91lin@gmail.com>
2021-08-12 16:00:06 +0800

    edit 'it'

--- test/url.js
+++ test/url.js
@@ -3,11 +3,11 @@ const url = require('../lib/etl/url')
 describe('url.join()', function() {
     const auth = 'TEST-AUTH'
 
-    it('shoud join url, ', function() {
+    it('shoud join url', function() {
         url.join('F-C0032-001', auth).should.equal('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=TEST-AUTH')
     })
 
-    it('shoud join url Array, ', function() {
+    it('shoud join Array of url', function() {
         url.joins(['F-C0032-001'], auth).should.eql(['https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=TEST-AUTH'])
     })
 })
